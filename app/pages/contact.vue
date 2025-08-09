ad482c39ae51d5d97e2c3ee29df69a6d7a585ab6
farzeen-ali <farzeenaliarif786@gmail.com>
2025-08-09 23:05:24 +0500

    File Based Routing

---
+++ app/pages/contact.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Contact Us Page</h2>
+         <NuxtLink to="/">Go to Home</NuxtLink>
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home Page</h2>
+        <!-- <a href="/contact">Contact</a> -->
+         <NuxtLink to="/contact">Contact</NuxtLink>
+    </div>
+</template>
