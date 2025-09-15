c0e45208ef24cc26f8495b4c1277af9e57f24088
farzeen-ali <farzeenaliarif786@gmail.com>
2025-09-15 22:39:05 +0500

    Life Cycle Hooks

---
+++ app/pages/life-cycle.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, onMounted, onUpdated, onBeforeUnmount } from 'vue';
+
+const count = ref(0);
+
+onMounted(() => {
+    console.log('Component Mounted!')
+})
+
+onUpdated(() => {
+    console.log('Component Updated!')
+})
+
+onUnmounted(() => {
+    console.log('Component Unmounted!')
+})
+
+function increment(){
+    count.value++;
+}
+
+</script>
+
+<template>
+    <div>
+        <h2>Life Cycle Hooks</h2>
+        <p>Count: {{ count }} </p>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
