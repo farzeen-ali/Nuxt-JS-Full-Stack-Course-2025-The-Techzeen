3f78e62073d64d11efddc618d91c2d305228dadf
farzeen-ali <farzeenaliarif786@gmail.com>
2025-09-23 20:17:28 +0500

    Composables - Logic Extraction

---
+++ app/pages/comp.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const { count, increment, decrement } = useCounter()
+</script>
+
+<template>
+    <div>
+        <h2>Counter using Composables</h2>
+        <p>Count: {{ count }} </p>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+    </div>
+</template>
